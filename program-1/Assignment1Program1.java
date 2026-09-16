e34cacdd94a2443d53b8a7b67400ace99b37a34a
Sitara <sswaby@hartford.edu>
2026-09-16 15:49:52 -0400

    Fundamentals Program 1

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class Assignment1Program1 {
   public static void main(String[] args) {
 
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("SSSSSSSSS   SSSSSSSSS");
+    System.out.println("SSS         SSS");
+    System.out.println("SSS         SSS");
+    System.out.println("SSSSSSSSS   SSSSSSSSS");
+    System.out.println("      SSS         SSS");
+    System.out.println("      SSS         SSS");
+    System.out.println("SSSSSSSSS   SSSSSSSSS");
+
+    System.out.print("Please enter a 5-character string: ");
+    String input = scanner.nextLine();
+
+    String middle = input.substring(1, 4);
+    String reversed = "";
+    reversed = reversed + middle.charAt(2) + middle.charAt(1) + middle.charAt(0);
+
+    System.out.print("Please enter the temperature in Fahrenheit: ");
+    double fahrenheit = scanner.nextDouble();
+    double celsius = (fahrenheit - 32) * 5 / 9;
+
+    System.out.println("Random number generated. Continuing...");
+    Random generator = new Random();
+    int randomNumber = generator.nextInt(16353) + 32;
+
+    System.out.println("Your new string is: " + celsius + reversed + randomNumber);
   }
 }
--- program-2/Assignment1Program2.java
+++ program-2/Assignment1Program2.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class Assignment1Program2 {
   public static void main(String[] args) {
 
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Base Conversion Program");
+    System.out.println();
+
+    System.out.print("Please enter a base (2 - 9): ");
+    int base = scanner.nextInt();
+
+    int maxNumber =
+        (base - 1) * base * base * base +
+        (base - 1) * base * base +
+        (base - 1) * base +
+        (base - 1);
+
+    System.out.println("The maximum, 4-digit, base 10 number in base "+ base + " is " + maxNumber + ".");
+    System.out.print("Now, enter a base 10 number in the range 0 to "+ maxNumber + " to convert: ");
+    int number = scanner.nextInt();
+
+    int first = number / (base * base * base);
+    int remainder = number % (base * base * base);
+
+    int second = remainder / (base * base);
+    remainder = remainder % (base * base);
+
+    int third = remainder / base;
+    int fourth = remainder % base;
+
+    System.out.println();
+    System.out.println(number + " (base 10) = "+ first + second + third + fourth + " (base " + base + ")");
   }
 }
+
